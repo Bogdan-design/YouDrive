641567acc6f743cd9eab7e13199da4e9eb77b668
Bogdan-design <bogdanbw@gmail.com>
2024-06-07 14:19:00 +0200

    header component

--- components/test/test.tsx
+++ components/test/test.tsx
@@ -5,25 +5,5 @@ const USERS_URL = 'https://example.com/api/users';
 export default function Table () {
     const [users, setUsers] = useState([])
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch(USERS_URL)
 
-                if (!res.ok) {
-                    throw new Error('Something went wrong')
-                }
-
-                const data = await res.json()
-
-                setUsers(data)
-            } catch (error) {
-                console.error('Error:', error)
-            }
-        };
-
-        fetchData()
-    }, []);
-
-    return
 };
