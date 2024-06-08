c438af5d198a2922dd41351a24adafe377efbca1
Bogdan-design <bogdanbw@gmail.com>
2024-06-08 23:01:05 +0200

    header component

--- components/test/test.tsx
+++
@@ -1,9 +0,0 @@
-import {useEffect, useState} from "react";
-
-const USERS_URL = 'https://example.com/api/users';
-
-export default function Table () {
-    const [users, setUsers] = useState([])
-
-
-};
