59a5b971089e6df7bfe6bdfb31ec4726323a76dc
Bogdan-design <bogdanbw@gmail.com>
2024-06-28 21:42:50 +0200

    header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -14,6 +14,7 @@ const Header = () => {
             <button>Shopify</button>
             <button>Shopify</button>
             <button>Shopify</button>
+            <button>Shopify</button>
         </section>
     );
 };
